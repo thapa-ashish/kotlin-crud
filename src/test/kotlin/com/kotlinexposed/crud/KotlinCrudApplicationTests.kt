a43002bb9bdfc99ec558da8d5d845d6644e93217
thapa-ashish <ashishthapa000@gmail.com>
2022-06-19 23:30:59 -0400

    first commit

---
+++ src/test/kotlin/com/kotlinexposed/crud/KotlinCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinexposed.crud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
